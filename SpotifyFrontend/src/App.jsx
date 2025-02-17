b4cf89f74d6a4db2d7aa8088eab8c3240c4c54ba
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-02-17 22:56:45 +0000

    Redoing the frontend

--- SpotifyFrontend/src/App.jsx
+++ SpotifyFrontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import UsersList from "./UserList.jsx"; // Import UsersList component
-import NavigationBar from "./NavigationBar.jsx"; // Import UsersList component
+
 import './App.css'
 const App = () => {
   return (
     <div>
-      <NavigationBar/>
+      
     </div>
   );
 };
