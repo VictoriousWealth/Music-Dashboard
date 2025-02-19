54b24d875e6bb61ba459cdf17660e931176d3feb
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-02-19 03:39:03 +0000

    Wow progress and playing around and making no progress beautifyl nuck

--- SpotifyFrontend/src/App.jsx
+++ SpotifyFrontend/src/App.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import TopBar from './MainComponents/TopBar.jsx'
-import SideBar from './MainComponents/SideBar.jsx'
-import CentreBar from './MainComponents/CentreBar.jsx'
 
 import './App.css'
 
 const App = () => {
   return (
     <div className="app">
-      <TopBar/>
-      <div className="not-top-bar">
-        <SideBar/>
-        <CentreBar/>
-      </div>
       
     </div>
   );
